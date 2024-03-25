7bc0068d86014afc96789d4aad20ef9e0548126f
JesseBot <jessebot@linux.com>
2024-03-25 12:05:47 +0100

    Delete test-locally.sh - no longer relevant

--- test-locally.sh
+++
@@ -1,3 +0,0 @@
-kind create cluster
-kubectl apply -f https://raw.githubusercontent.com/longhorn/longhorn/v1.6.0/deploy/longhorn.yaml
-ct install --target-branch main --helm-extra-set-args "--set=mq.storageClassName=longhorn --set=dns.storageClassName=longhorn"
